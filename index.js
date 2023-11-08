01f8d7b1a2df67f8ea7482eff95367a95f64ae53
Kevin Foster <42094092+kevfoste@users.noreply.github.com>
2023-11-08 10:15:36 -0500

    Updated calling functions

--- index.js
+++ index.js
@@ -7,27 +7,29 @@
  */
 
  // Global Init 
- 
+const util = require('util');
 const debugging = true;
 const { Octokit } = require("@octokit/core");
 const { createProbotAuth, authStrategy } = require("octokit-auth-probot");
 const { config, composeConfigGet } = require("@probot/octokit-plugin-config");
 
-// Load the .env file vars
-const APP_ID = process.env.APP_ID;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+// // Load the .env file vars
+// const APP_ID = process.env.APP_ID;
+// const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-// Authenticate with the app ID and private key
-const ProbotOctokit = Octokit.defaults({
-    authStrategy: createProbotAuth,
-});
+// // Authenticate with the app ID and private key
+// const ProbotOctokit = Octokit.defaults({
+//     authStrategy: createProbotAuth,
+// });
 
-const octokit = new ProbotOctokit({
-    auth: {
-      appId: APP_ID,
-      privateKey: PRIVATE_KEY,
-    },
-  });
+// const octokit = new ProbotOctokit({
+//     auth: {
+//       appId: APP_ID,
+//       privateKey: PRIVATE_KEY,
+      
+//     },
+//   });
+
 
 var hasInvalidTypesBool = false;
 var invalidfileTypesFound = [];
@@ -38,7 +40,7 @@ module.exports = (app) => {
   app.on(
     ["pull_request.opened", "pull_request.synchronize"],
     async (context) => {
-//      app.log.info(context.payload);
+      //app.log.info(context);
 
       // Init global vars
       hasInvalidTypesBool = false;
@@ -53,17 +55,17 @@ module.exports = (app) => {
       // Could also keep file extension array 
       // in the .env file and load from 
       // there to prevent using a token and a separate repo
-      let config = {};
       try {
-        config  = await octokit.config.get({
+        configResult  = await context.octokit.config.get({
           owner: process.env.LOAD_CONFIG_OWNER,
           repo: process.env.LOAD_CONFIG_REPO,
           path: process.env.LOAD_CONFIG_PATH
       });
       } catch (error) {
-        console.log(`Error loading the freaking config file: ${error.message}`);
+        console.log(`Error loading config file: ${error.message}`);
       }
-
+      
+      if (debugging) { console.log(` ConfigResult is:` + util.inspect(configResult) ); }
       // Get the files modified by the commits in the pull request
       let res = {};
       try {
@@ -86,14 +88,14 @@ module.exports = (app) => {
         var invalidfileTypes = new Array();
 
         if (debugging) { console.log(`laststring is: ${laststring}`); }
-        if (debugging) { console.log(`config invalidfileTypes is: ${config.invalidfileTypes}`); }
+        if (debugging) { console.log(`config invalidfileTypes is: ${configResult.config.invalidfileTypes}`); }
 
         // Load the invalidfileTypes from the .env file or the config file
         if (process.env.invalidfileTypes) {
             invalidfileTypes = process.env.invalidfileTypes.split(','); 
             if (debugging) { console.log(`invalidfileTypes from .env is: ${invalidfileTypes}`); }
         } else {
-            invalidfileTypes = config.invalidfileTypes;
+            invalidfileTypes = configResult.config.invalidfileTypes;
             if (debugging) { console.log(`invalidfileTypes from config is: ${invalidfileTypes}`); }
         }
 
